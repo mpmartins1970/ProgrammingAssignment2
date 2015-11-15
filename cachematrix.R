be954f434a3d1b583ff86fbdce249abacd6f49c8
mpmartins1970 <marcelo.martins@neogrid.com>
2015-11-15 10:40:33 -0200

    Second Commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,18 +4,31 @@
 
 # Creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
+        
         s <- NULL
+        
         set <- function(y) {
                 x <<- y
                 s <<- NULL
         }
-        get <- function() x
-        setSolve <- function(solve) s <<- solve
-        getSolve <- function() s
+        
+        get <- function() { 
+                x
+        }
+        
+        setSolve <- function(solve) {
+                s <<- solve
+        }
+        
+        getSolve <- function() {
+                s
+        }
+        
         list(set = set, 
              get = get,
              setSolve = setSolve,
              getSolve = getSolve)
+        
 }
 
 
@@ -35,5 +48,5 @@ cacheSolve <- function(x, ...) {
         
         ## Return a matrix that is the inverse of 'x'
         s
-
+        
 }
